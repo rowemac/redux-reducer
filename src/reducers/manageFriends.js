7e52122ee3f576cf8306a1d35b5039d2c0cf7650
Mac Rowe <m.rowe42@gmail.com>
2021-07-05 15:48:43 -0400

    commit tree 13007fa571465e82791e3eb76a4d230e385f122e

--- src/reducers/manageFriends.js
+++ src/reducers/manageFriends.js
@@ -1,2 +1,20 @@
-export function manageFriends(state, action){
+export function manageFriends(state, action) {
+    let stateCpy = {friends:[]}
+    if(state.friends) {
+      state.friends.map( friend => {
+        stateCpy.friends.push( Object.assign({},friend) );
+      });
+    }
+  
+    switch(action.type) {
+      case "ADD_FRIEND":
+        stateCpy.friends.push( Object.assign({},action.friend) );
+        return stateCpy;
+      case "REMOVE_FRIEND":
+        stateCpy.friends = stateCpy.friends.filter( friend => friend.id !== action.id );
+        return stateCpy;
+      default:
+        return stateCpy;
+    }
 }
+  
--- src/reducers/managePresents.js
+++ src/reducers/managePresents.js
@@ -1,2 +1,8 @@
 export function managePresents(state, action){
-}
+    switch(action.type) {
+      case "INCREASE":
+        return Object.assign({}, state, {numberOfPresents: state.numberOfPresents+1 });
+      default:
+        return Object.assign({},state);
+    }
+  }
